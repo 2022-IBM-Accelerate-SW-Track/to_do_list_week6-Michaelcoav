3fd249baaf9db9d71d66b58567a779f3790997c4
Michaelcoav <michaelcoav@gmail.com>
2022-07-20 20:32:45 -0400

    Addition of login page to do list

--- backend/authentication.js
+++ backend/authentication.js
@@ -3,7 +3,13 @@ var fs = require('fs');
 const basicAuth = require("express-basic-auth");
 
 // users holds user information that is backed up on file
-let users = {};
+let users = {
+    "users": {
+        "user": {
+            "passwordHash": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+        }
+    }
+};
 
 // initialize users store from file
 (() => {
